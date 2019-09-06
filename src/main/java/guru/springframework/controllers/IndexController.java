e9983cac86c8067e2a987cc168af525f976604cf
giulimarket <giuli.market@gmail.com>
2019-09-06 14:23:18 +0200

    Lezione 147

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,14 @@
 package guru.springframework.controllers;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
@@ -15,8 +16,26 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", "/index"})
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+	
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
+
+
+	@RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
-        return "index";
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Americanaaaa");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		
+        System.out.println("Category id: " + (categoryOptional.isPresent() ? categoryOptional.get().getId() : "non trovato"));
+        
+        System.out.println("UOM id: " + (unitOfMeasureOptional.isPresent() ?unitOfMeasureOptional.get().getId() : "non trovato"));
+        
+        
+		return "index";
     }
 }
